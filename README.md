2630379853bdc0d6c9d73b408a7e8da8f940199a
h2oche <h2oche22@gmail.com>
2019-05-03 15:55:59 +0900

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-﻿#BOBO
-For Instructors.
---
+++ precache-manifest.8e085d45abffba2e35bf3db08491826f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194bccf755b0394583f6a11c77593d33",
+    "url": "/BOBO/index.html"
+  },
+  {
+    "revision": "c619660b36c58278cf37",
+    "url": "/BOBO/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/BOBO/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c619660b36c58278cf37",
+    "url": "/BOBO/static/js/main.3f8d8bc5.chunk.js"
+  },
+  {
+    "revision": "d5885e5e58f947cf402e",
+    "url": "/BOBO/static/js/runtime~main.8e648452.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BOBO/static/media/logo.5d5d9eef.svg"
+  }
+]);
